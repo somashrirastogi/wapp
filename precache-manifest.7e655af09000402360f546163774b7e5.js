f42b3c2673af0f81a0da24c5e51d75191a727571
Somashri rastogi <somashri.19b131102@abes.ac.in>
2021-05-17 16:28:19 +0530

    Updates

---
+++ precache-manifest.7e655af09000402360f546163774b7e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66855d5e1893584d1261bac8a06bb89",
+    "url": "/wapp/index.html"
+  },
+  {
+    "revision": "3e0ab926a9dd4bb5e9c9",
+    "url": "/wapp/static/css/2.4898611b.chunk.css"
+  },
+  {
+    "revision": "c5cdf9ddc86cfbc89677",
+    "url": "/wapp/static/css/main.20e57722.chunk.css"
+  },
+  {
+    "revision": "3e0ab926a9dd4bb5e9c9",
+    "url": "/wapp/static/js/2.6023ccc5.chunk.js"
+  },
+  {
+    "revision": "c5cdf9ddc86cfbc89677",
+    "url": "/wapp/static/js/main.4bdb04de.chunk.js"
+  },
+  {
+    "revision": "b91fe52654fff238d18f",
+    "url": "/wapp/static/js/runtime~main.5a2ba641.js"
+  },
+  {
+    "revision": "1274464865ca48a0c092d14ad2448c69",
+    "url": "/wapp/static/media/i.12744648.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/wapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/wapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/wapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/wapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/wapp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
